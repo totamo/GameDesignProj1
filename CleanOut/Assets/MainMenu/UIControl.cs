748a053b0bacc2d2eb2ab15630d680504b28b526
FlaxenTrousers <langolier009@gmail.com>
2017-02-07 23:06:35 -0600

    Add files via upload

---
+++ CleanOut/Assets/MainMenu/UIControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+
+    public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
